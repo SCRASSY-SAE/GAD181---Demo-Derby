37c8f1cc416a11b09d4375294d0b5595e78e6f9d
SAE1\1033487 <1033487@student.sae.edu.au>
2024-07-30 15:16:34 +1000

    Updated car mechanics, Timer added.

--- Assets/Cayden/Scripts/PlayerHUD.cs
+++ Assets/Cayden/Scripts/PlayerHUD.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHUD : MonoBehaviour
 {
     public Transform healthBar;
     public Transform playerCar;
+    [SerializeField] TextMeshPro timerText;
 
     float playerMaxHealth;
+    public float timePassed;
 
     void UpdatePlayerHealthBar()
     {
@@ -17,16 +20,29 @@ public class PlayerHUD : MonoBehaviour
 
         healthBar.GetComponent<Slider>().value = playerHealth / playerMaxHealth;
     }
+    void UpdateTimer()
+    {
+
+        timePassed += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timePassed / 60);
+        int seconds = Mathf.FloorToInt(timePassed % 60);
+        int milliseconds = (int)(timePassed * 1000) % 1000;
+        //timerText.text = timePassed.ToString();
+        timerText.text = $"{minutes}:{seconds}.{milliseconds}";
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         playerMaxHealth = playerCar.GetComponent<DamageCar>().vehicleHealth;
+        
+        timePassed = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdatePlayerHealthBar();
+        UpdateTimer();
     }
 }
